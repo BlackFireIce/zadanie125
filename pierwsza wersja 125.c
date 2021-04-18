912065a68e653a80c705cfb1cd8c23639d33cd28
mateusz sieczkowski <tadekjezus@gmail.com>
2021-04-18 16:18:16 +0200

    dodanie objetosci ostroslupa

--- pierwsza wersja 125.c
+++ pierwsza wersja 125.c
@@ -1,34 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
+#include <math.h>
 
 
 int main()
 {
-    for(;;)
-    {
-        float a;
-        float b;
-        float wynik;
-        float pods;
-        float h;
-        float wynik2;
-        float t;
-        printf("Objetosc prostopadloscianu\n");
+for(;;)
+{
+int c=68 ;
+float a, b, wynik, h, wynik2, t, pole_podstawy;
+printf("wybierz operacje\n");
+printf("1.objetosc prostopadloscianu\n");
+printf("2.objetosc ostroslupa prawidlowego czworokatnego\n");
+printf("3. wyjscie\n");
+scanf("%d", &c);
+switch (c)
 
+{
 
-    printf("wprowadz dlugosc boku a\n");
+case 1:
+
+    printf("wprowadz dlugosc boku a:\n");
     scanf("%f", &a);
-    printf("wprowadz dlugosc boku b\n");
+    printf("wprowadz dlugosc boku b:\n");
     scanf("%f", &b);
-    printf("wprowadz wysokosc h\n");
+    printf("wprowadz wysokosc h:\n");
     scanf("%f", &h);
+
     wynik = a*b*h;
     printf("wynik=" "%f\n", wynik);
 
-    return 0;
-    }
+    break;
+case 2:
+
+    printf("wprowadz dlugosc boku a:\n");
+    scanf("%f", &a);
+    pole_podstawy = a*a;
+
+    printf("pole_podstawy=" "%f\n", pole_podstawy);
+
+    printf("wprowadz wysokosc h:\n");
+    scanf("%f", &h);
+
+    wynik2 = pole_podstawy * h / 3;
+    printf("wynik=" "%f\n", wynik2);
+    break;
+case 3:
+   return 0;
+default:
+  printf("Zly wybor\n");
 }
 
 
+}
+    return 0;
+
+}
