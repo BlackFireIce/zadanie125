3693cbf07fa8d950569dc4315abc51f0bec7aa33
Michal Joozefiak <michal.jozefiak24@gmail.com>
2021-04-18 20:23:48 +0200

    Gotowa wersja v2

--- pierwsza wersja 125.c
+++ pierwsza wersja 125.c
@@ -5,55 +5,55 @@
 
 int main()
 {
-for(;;)
-{
-int c=68 ;
-float a, b, wynik, h, wynik2, t, pole_podstawy;
-printf("wybierz operacje\n");
-printf("1.objetosc prostopadloscianu\n");
-printf("2.objetosc ostroslupa prawidlowego czworokatnego\n");
-printf("3. wyjscie\n");
-fflush (stdin);
-scanf("%d", &c);
-switch (c)
+    for(;;)
+    {
+    int c=68 ;
+    float a, b, wynik, h, wynik2, t, pole_podstawy;
 
-{
+        printf("wybierz operacje\n");
+        printf("1.objetosc prostopadloscianu\n");
+        printf("2.objetosc ostroslupa prawidlowego czworokatnego\n");
+        printf("3. wyjscie\n");
+        fflush (stdin);
+        scanf("%d", &c);
+        switch (c)
 
-case 1:
+        {
 
-    printf("wprowadz dlugosc boku a:\n");
-    scanf("%f", &a);
-    printf("wprowadz dlugosc boku b:\n");
-    scanf("%f", &b);
-    printf("wprowadz wysokosc h:\n");
-    scanf("%f", &h);
+        case 1:
 
-    wynik = a*b*h;
-    printf("wynik=" "%f\n", wynik);
+            printf("wprowadz dlugosc boku a:\n");
+            scanf("%f", &a);
+            printf("wprowadz dlugosc boku b:\n");
+            scanf("%f", &b);
+            printf("wprowadz wysokosc h:\n");
+            scanf("%f", &h);
+            printf("Nieprawidlowa wartosc\n");
+            wynik = a*b*h;
+            printf("wynik=" "%f\n", wynik);
+        break;
 
-    break;
-case 2:
+        case 2:
 
-    printf("wprowadz dlugosc boku a:\n");
-    scanf("%f", &a);
-    pole_podstawy = a*a;
+            printf("wprowadz dlugosc boku a:\n");
+            scanf("%f", &a);
+            pole_podstawy = a*a;
+            printf("pole_podstawy=" "%f\n", pole_podstawy);
+            printf("wprowadz wysokosc h:\n");
+            scanf("%f", &h);
+            printf("Nieprawidlowa wartosc\n");
+            wynik2 = pole_podstawy * h / 3;
+            printf("wynik=" "%f\n", wynik2);
+        break;
 
-    printf("pole_podstawy=" "%f\n", pole_podstawy);
-
-    printf("wprowadz wysokosc h:\n");
-    scanf("%f", &h);
-
-    wynik2 = pole_podstawy * h / 3;
-    printf("wynik=" "%f\n", wynik2);
-    break;
-case 3:
-   return 0;
-default:
-  printf("Zly wybor\n");
-}
+        case 3:
+            return 0;
+            default:
+                printf("Zly wybor\n");
+        }
 
 
-}
+    }
     return 0;
 
 }
